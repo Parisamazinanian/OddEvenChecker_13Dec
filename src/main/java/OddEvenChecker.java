314a15160572520dc747a684333d767ca1836db5
Parisamazinanian <par.mazinanian@gmail.com>
2021-12-13 14:04:41 +0100

    OddEven Done

---
+++ src/main/java/OddEvenChecker.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class OddEvenChecker {
+    /**
+     * This method verifies if the given number is odder number or not.
+     *
+     * @param number
+     * @return
+     */
+    public boolean isOddNumber(int number) {
+        return number%2 != 0;
+    }
+}
---
+++ src/test/java/OddEvenCheckerTest.java
@@ -0,0 +1,29 @@
+package test.java;
+
+import main.java.OddEvenChecker;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OddEvenCheckerTest {
+
+    private OddEvenChecker oddEvenChecker = new OddEvenChecker();
+
+
+    @ParameterizedTest(name = "test odd number with: {arguments} ")
+    @ValueSource(ints={25,15})
+            public void testOddNumber(int number) {
+        assertTrue(oddEvenChecker.isOddNumber(number));
+
+    }
+
+
+    @ParameterizedTest(name = "test even number with: {arguments} ")
+    @ValueSource(ints={20,10})
+            public void testEvenNumber(int number) {
+        assertFalse(oddEvenChecker.isOddNumber(number));
+    }
+}
